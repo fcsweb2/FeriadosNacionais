9fbba15fe5b99b37689a421dc37890119f869e16
Fernando Cardoso <fcsweb2@gmail.com>
2016-11-27 23:21:36 -0200

    Mudar nome para FeriadoBR

---
+++ FeriadosBR/FeriadoFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeriadosBR
+{
+    class FeriadoFactory
+    {
+        public async void GetRequest()
+        {
+            Uri geturi = new Uri("http://feriado.hcfsolutions.com.br/app/feriados");
+            HttpClient client = new HttpClient();
+            HttpResponseMessage responseGet = await client.GetAsync(geturi);
+
+            string response = await responseGet.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ FeriadosBR/Model/Feriado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeriadosBR
+{
+    class Feriado
+    {
+        public string data { get; set; }
+        public string nome { get; set; }
+        public string obs { get; set; }
+        public string diaSemana { get; set; }
+        public string diaSemanaDescricao { get; set; }
+        public int id { get; set; }
+        public int intervalo { get; set; }
+    }
+}
